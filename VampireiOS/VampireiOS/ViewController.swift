1ea4e1c0dd6ed04147af172550ceabe568ee69d6
Adan Cervera <adan.cervera@gmail.com>
2020-05-25 21:26:29 -0500

    initial commit
    initial commit

---
+++ VampireiOS/VampireiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VampireiOS
+//
+//  Created by Adan Cervera on 25/05/20.
+//  Copyright © 2020 Adan Cervera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
